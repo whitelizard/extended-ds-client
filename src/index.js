d153b828a74566ee0fc293d98ae1f907b4b4ad1d
Esbjörn Blomquist <esbjorn.blomquist@cybercom.com>
2017-07-14 19:54:40 +0200

    added has and setExistingRecordP

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
 import deepstream from 'deepstream.io-client-js';
+import merge from 'lodash.merge';
 
 export const statuses = deepstream.CONSTANTS.CONNECTION_STATE;
 
-// TODO: Make Symbols out of data model parts/tags ?
-
-/**
- * Simple object check.
- * @param item
- * @returns {boolean}
- */
-export function isObject(item) {
-  return item && typeof item === 'object' && !Array.isArray(item);
-}
-
-/**
- * Deep merge two objects.
- * @param target
- * @param ...sources
- */
-export function mergeDeep(target, ...sources) {
-  if (!sources.length) return target;
-  const source = sources.shift();
-
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) Object.assign(target, { [key]: {} });
-        mergeDeep(target[key], source[key]);
-      } else {
-        Object.assign(target, { [key]: source[key] });
-      }
-    }
-  }
-  return mergeDeep(target, ...sources);
-}
-
 export function addEntry(list, str) {
-  if (list.getEntries().indexOf(str) > -1) return;
+  if (list.getEntries().indexOf(str) > -1) return undefined;
   return list.addEntry(str);
 }
 
@@ -67,7 +35,7 @@ function snapshotP(name) {
   );
 }
 
-function listedRecordP(listPath, recordId, obj, overwrite) {
+function listedRecordP(listPath, recordId, obj, deepMerge, overwrite) {
   const id = recordId || this.getUid();
   const rPath = `${listPath}/${id}`;
   return Promise.all([
@@ -83,12 +51,28 @@ function listedRecordP(listPath, recordId, obj, overwrite) {
     if (Object.keys(r).length === 0) {
       record.set(newRecord);
       created = true;
+    } else if (deepMerge) {
+      record.set(merge(r, obj));
     } else if (overwrite) {
-      Object.keys(newRecord).forEach(key => record.set(key, newRecord[key]));
+      record.set(newRecord);
     } else {
-      record.set(mergeDeep(r, obj));
+      Object.keys(newRecord).forEach(key => record.set(key, newRecord[key]));
     }
     return [id, created];
+  });
+}
+
+function setExistingRecordP(name, obj, deepMerge, overwrite) {
+  return this.record.getExistingRecordP(name).then(record => {
+    if (deepMerge) {
+      const r = record.get();
+      record.set(merge(r, obj));
+    } else if (overwrite) {
+      record.set(obj);
+    } else {
+      Object.keys(obj).forEach(key => record.set(key, obj[key]));
+    }
+    return record;
   });
 }
 
@@ -101,13 +85,24 @@ function loginP(authParams) {
   );
 }
 
+function hasP(name) {
+  return new Promise((resolve, reject) =>
+    this.record.has(name, (error, hasRecord) => {
+      if (error) reject(new Error(error));
+      else resolve(hasRecord);
+    }),
+  );
+}
+
 export default function getClient(url, options) {
   const c = deepstream(url, options);
   c.record.getRecordP = getRecordP.bind(c);
   c.record.getListP = getListP.bind(c);
   c.record.getExistingRecordP = getExistingP.bind(c, 'Record');
   c.record.getExistingListP = getExistingP.bind(c, 'List');
+  c.record.setExistingRecordP = setExistingRecordP.bind(c);
   c.record.snapshotP = snapshotP.bind(c);
+  c.record.hasP = hasP.bind(c);
   c.record.listedRecordP = listedRecordP.bind(c);
   c.loginP = loginP.bind(c);
   return c;
@@ -126,13 +121,14 @@ export function getClientWithTenant(url, options, tenant = 'demo') {
   c.record.getRecordT = withTenant.bind(c, 'getRecord');
   c.record.getListPT = withTenant.bind(c, 'getListP');
   c.record.getListT = withTenant.bind(c, 'getList');
-  c.record.getExistingRecordPT = withTenant.bind(c, 'getExistingRecordP');
-  c.record.getExistingRecordT = withTenant.bind(c, 'getExistingRecord');
-  c.record.getExistingListPT = withTenant.bind(c, 'getExistingListP');
-  c.record.getExistingListT = withTenant.bind(c, 'getExistingList');
   c.record.snapshotPT = withTenant.bind(c, 'snapshotP');
   c.record.snapshotT = withTenant.bind(c, 'snapshot');
+  c.record.hasPT = withTenant.bind(c, 'hasP');
+  c.record.hasT = withTenant.bind(c, 'has');
+  c.record.getExistingRecordPT = withTenant.bind(c, 'getExistingRecordP');
+  c.record.getExistingListPT = withTenant.bind(c, 'getExistingListP');
   c.record.listedRecordPT = withTenant.bind(c, 'listedRecordP');
+  c.record.setExistingRecordPT = withTenant.bind(c, 'setExistingRecordP');
   return c;
 }
 
