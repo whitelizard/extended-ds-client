f6e0ef4426843e752fa175e2c5e2668018829ee8
Esbjörn Blomquist <esbjorn.blomquist@cybercom.com>
2017-11-02 20:19:42 +0100

    tests converted to async/await and using t for better printout

--- test/index.js
+++ test/index.js
@@ -32,232 +32,194 @@ test('CONSTANTS', t => {
   t.end();
 });
 
-test('p.login', () => Promise.all([c.p.login({}), cc.loginP({})]));
+test('p.login', async t => {
+  await Promise.all([c.p.login({}), cc.loginP({})]);
+  t.ok(true);
+});
 
 test('provide', t => {
   c.rpc.provide('rpc1', (err, res) => res.send(5));
   t.end();
 });
 
-test('p.getRecord', () =>
-  c.record.p.getRecord('record1').then(r => {
-    if (Object.keys(r.get()).length !== 0) throw new Error('Not empty object');
-    r.set({ name: 'Record1', data: { a: 1 } });
-    return true;
-  }));
-test('getRecordP', () =>
-  c.record.getRecordP('record1').then(r => {
-    if (Object.keys(r.get()).length !== 2) throw new Error('Bad object');
-    return true;
-  }));
+test('p.make', async t => {
+  const res = await c.rpc.p.make('rpc1');
+  t.ok(res === 5);
+});
+test('makeP', async t => {
+  const res = await c.rpc.makeP('rpc1');
+  t.ok(res === 5);
+});
 
-test('p.getList', () =>
-  c.record.p.getList('list1').then(l => {
-    if (l.getEntries().length !== 0) throw new Error('Not empty list');
-    addEntry(l, 'record1');
-    return true;
-  }));
-test('getListP', () =>
-  c.record.getListP('list1').then(l => {
-    addEntry(l, 'record1');
-    if (l.getEntries().length !== 1) throw new Error('Bad list');
-    return true;
-  }));
+test('p.getRecord', async t => {
+  const r = await c.record.p.getRecord('record1');
+  const rec = r.get();
+  t.ok(Object.keys(rec).length === 0);
+  r.set({ name: 'Record1', data: { a: 1 } });
+});
+test('getRecordP', async t => {
+  const r = await c.record.getRecordP('record1');
+  const rec = r.get();
+  t.ok(Object.keys(rec).length === 2);
+});
 
-test('p.snapshot', () =>
-  c.record.p.snapshot('record1').then(r => {
-    console.log(r);
-    if (JSON.stringify(r) !== '{"name":"Record1","data":{"a":1}}') throw new Error('Bad record');
-    return true;
-  }));
-test('snapshotP', () =>
-  c.record.snapshotP('record1').then(r => {
-    if (JSON.stringify(r) !== '{"name":"Record1","data":{"a":1}}') throw new Error('Bad record');
-    return true;
-  }));
+test('p.getList + addEntry', async t => {
+  const l = await c.record.p.getList('list1');
+  const list = l.getEntries();
+  t.ok(list.length === 0);
+  addEntry(l, 'record1');
+});
+test('getListP + addEntry no doubles', async t => {
+  const l = await c.record.getListP('list1');
+  addEntry(l, 'record1');
+  const list = l.getEntries();
+  t.ok(list.length === 1);
+});
+
+test('p.snapshot', async t => {
+  const r = await c.record.p.snapshot('record1');
+  console.log(r);
+  t.ok(JSON.stringify(r) === '{"name":"Record1","data":{"a":1}}');
+});
+test('snapshotP', async t => {
+  const r = await c.record.snapshotP('record1');
+  t.ok(JSON.stringify(r) === '{"name":"Record1","data":{"a":1}}');
+});
 test('p.snapshot fail', t => t.shouldFail(c.record.p.snapshot('record2')));
 
-test('p.has', () =>
-  c.record.p.has('record1').then(ok => {
-    if (!ok) throw new Error('Bad record');
-    return true;
-  }));
-test('hasP', () =>
-  c.record.hasP('record1').then(ok => {
-    if (!ok) throw new Error('Bad record');
-    return true;
-  }));
+test('p.has', async t => {
+  const ok = await c.record.p.has('record1');
+  t.ok(ok);
+});
+test('hasP', async t => {
+  const ok = await c.record.hasP('record1');
+  t.ok(ok);
+});
 
-test('p.getExistingRecord', () =>
-  c.record.p.getExistingRecord('record1').then(r => {
-    if (r.get().name !== 'Record1') throw new Error('Bad record');
-    return true;
-  }));
-test('getExistingRecordP', () =>
-  c.record.getExistingRecordP('record1').then(r => {
-    if (r.get().name !== 'Record1') throw new Error('Bad record');
-    return true;
-  }));
+test('p.getExistingRecord', async t => {
+  const r = await c.record.p.getExistingRecord('record1');
+  t.ok(r.get().name === 'Record1');
+});
+test('getExistingRecordP', async t => {
+  const r = await c.record.getExistingRecordP('record1');
+  t.ok(r.get().name === 'Record1');
+});
 test('p.getExistingRecord fail', t => t.shouldFail(c.record.p.getExistingRecord('record2')));
 
-test('p.getExistingList', () =>
-  c.record.p.getExistingList('list1').then(l => {
-    if (l.getEntries()[0] !== 'record1') throw new Error('Bad list');
-    return true;
-  }));
-test('getExistingListP', () =>
-  c.record.getExistingListP('list1').then(l => {
-    if (l.getEntries()[0] !== 'record1') throw new Error('Bad list');
-    return true;
-  }));
+test('p.getExistingList', async t => {
+  const l = await c.record.p.getExistingList('list1');
+  t.ok(l.getEntries()[0] === 'record1');
+});
+test('getExistingListP', async t => {
+  const l = await c.record.getExistingListP('list1');
+  t.ok(l.getEntries()[0] === 'record1');
+});
 
-test('p.addToList', () =>
-  c.record.p.addToList('list1', 'record2').then(l => {
-    if (l.getEntries()[1] !== 'record2') throw new Error('Bad list');
-    return true;
-  }));
-test('addToListP', () =>
-  c.record.addToListP('list1', 'record3').then(l => {
-    if (l.getEntries()[2] !== 'record3') throw new Error('Bad list');
-    return true;
-  }));
+test('p.addToList', async t => {
+  const l = await c.record.p.addToList('list1', 'record2');
+  t.ok(l.getEntries()[1] === 'record2');
+});
+test('addToListP', async t => {
+  const l = await c.record.addToListP('list1', 'record3');
+  t.ok(l.getEntries()[2] === 'record3');
+});
 
-test('p.removeFromList', () =>
-  c.record.p.removeFromList('list1', 'record2').then(l => {
-    if (JSON.stringify(l.getEntries()) !== '["record1","record3"]') throw new Error('Bad list');
-    return true;
-  }));
-test('removeFromListP', () =>
-  c.record.removeFromListP('list1', 'record3').then(l => {
-    if (JSON.stringify(l.getEntries()) !== '["record1"]') throw new Error('Bad list');
-    return true;
-  }));
+test('p.removeFromList', async t => {
+  const l = await c.record.p.removeFromList('list1', 'record2');
+  t.ok(JSON.stringify(l.getEntries()) === '["record1","record3"]');
+});
+test('removeFromListP', async t => {
+  const l = await c.record.removeFromListP('list1', 'record3');
+  t.ok(JSON.stringify(l.getEntries()) === '["record1"]');
+});
 
-test('p.addToList multi', () =>
-  c.record.p.addToList('list1', ['record4', 'r5', 'r6']).then(l => {
-    if (l.getEntries()[3] !== 'r6') throw new Error('Bad list');
-    return true;
-  }));
-test('p.removeFromList multi', () =>
-  c.record.p.removeFromList('list1', ['record4', 'r5', 'r6']).then(l => {
-    if (JSON.stringify(l.getEntries()) !== '["record1"]') throw new Error('Bad list');
-    return true;
-  }));
+test('p.addToList multi', async t => {
+  const l = await c.record.p.addToList('list1', ['record4', 'r5', 'r6']);
+  t.ok(l.getEntries()[3] === 'r6');
+});
+test('p.removeFromList multi', async t => {
+  const l = await c.record.p.removeFromList('list1', ['record4', 'r5', 'r6']);
+  t.ok(JSON.stringify(l.getEntries()) === '["record1"]');
+});
 
-test('p.setExistingRecord + deepMerge', () =>
-  c.record.p.setExistingRecord('record1', { data: { b: 2 } }, true).then(r => {
-    if (JSON.stringify(r.get()) !== '{"name":"Record1","data":{"a":1,"b":2}}') {
-      throw new Error('Bad record');
-    }
-    return true;
-  }));
-test('setExistingRecordP + default update mode', () =>
-  c.record.setExistingRecordP('record1', { data: 5 }).then(r => {
-    if (JSON.stringify(r.get()) !== '{"name":"Record1","data":5}') {
-      throw new Error('Bad record');
-    }
-    return true;
-  }));
-test('p.setExistingRecord + overwrite', () =>
-  c.record.p.setExistingRecord('record1', { title: 'The Record' }, false, true).then(r => {
-    if (JSON.stringify(r.get()) !== '{"title":"The Record"}') {
-      throw new Error('Bad record');
-    }
-    return true;
-  }));
+test('p.setExistingRecord + deepMerge', async t => {
+  const r = await c.record.p.setExistingRecord('record1', { data: { b: 2 } }, true);
+  t.ok(JSON.stringify(r.get()) === '{"name":"Record1","data":{"a":1,"b":2}}');
+});
+test('setExistingRecordP + default update mode', async t => {
+  const r = await c.record.setExistingRecordP('record1', { data: 5 });
+  t.ok(JSON.stringify(r.get()) === '{"name":"Record1","data":5}');
+});
+test('p.setExistingRecord + overwrite', async t => {
+  const r = await c.record.p.setExistingRecord('record1', { title: 'The Record' }, false, true);
+  t.ok(JSON.stringify(r.get()) === '{"title":"The Record"}');
+});
 
-test('p.make', () =>
-  c.rpc.p.make('rpc1').then(res => {
-    if (res !== 5) throw new Error('Bad answer');
-    return true;
-  }));
-test('makeP', () =>
-  c.rpc.makeP('rpc1').then(res => {
-    if (res !== 5) throw new Error('Bad answer');
-    return true;
-  }));
+test('p.getListedRecord', async t => {
+  const [l, r] = await c.record.p.getListedRecord('records', 'record2', {
+    name: 'Record2',
+    data: [1],
+  });
+  const rec = r.get();
+  t.ok(rec.id === 'record2');
+  t.ok(rec.name === 'Record2');
+  t.ok(JSON.stringify(l.getEntries()) === '["records/record2"]');
+});
 
-test('p.getListedRecord', () =>
-  c.record.p
-    .getListedRecord('records', 'record2', { name: 'Record2', data: [1] })
-    .then(([l, r]) => {
-      if (r.get().id !== 'record2') throw new Error('Bad ID');
-      if (JSON.stringify(l.getEntries()) !== '["records/record2"]') throw new Error('Bad list');
-      if (r.get().name !== 'Record2') throw new Error('Bad record content');
-      return true;
-    }));
 let tId;
-test('setListedRecordP + auto id', () =>
-  cc.record.setListedRecordP('cars', undefined, { name: 'Number 3' }).then(id => {
-    if (typeof id !== 'string' && id.length !== c.getUid().length) {
-      throw new Error('Was not created!?');
-    }
-    tId = id;
-    return cc.record.getExistingListP('cars').then(l => {
-      if (JSON.stringify(l.getEntries()) !== `["${id}"]`) {
-        throw new Error('Bad list');
-      }
-      return cc.record.getExistingRecordP(`cars.${id}`).then(r => {
-        if (r.get().name !== 'Number 3') throw new Error('Bad record content');
-        return true;
-      });
-    });
-  }));
-test('p.getListedRecord deepMergeCustomizer', () =>
-  c.record.p
-    .getListedRecord(
-      'records',
-      'record2',
-      { data: [2] },
-      true,
-      undefined,
-      (d, s) => Array.isArray(d) && d.concat(s),
-    )
-    .then(([l, r]) => {
-      const rec = r.get();
-      console.log('record:', rec);
-      if (rec.data.length !== 2) throw new Error('Bad deep merge with concat');
-      l.discard();
-      return true;
-    }));
+test('setListedRecordP + auto id', async t => {
+  tId = await cc.record.setListedRecordP('cars', undefined, { name: 'Number 3' });
+  t.ok(typeof tId === 'string' && tId.length === c.getUid().length);
+  const l = await cc.record.getExistingListP('cars');
+  t.ok(JSON.stringify(l.getEntries()) === `["${tId}"]`);
+  const r = await cc.record.getExistingRecordP(`cars.${tId}`);
+  t.ok(r.get().name === 'Number 3');
+});
+test('p.getListedRecord deepMergeCustomizer', async t => {
+  const [l, r] = await c.record.p.getListedRecord(
+    'records',
+    'record2',
+    { data: [2] },
+    true,
+    undefined,
+    (d, s) => Array.isArray(d) && d.concat(s),
+  );
+  const rec = r.get();
+  t.ok(rec.data.length === 2);
+  l.discard();
+});
 
-test('p.deleteListedRecord', () =>
-  c.record.p.deleteListedRecord('records', 'record2').then(ok => {
-    if (!ok) throw new Error('Not ok');
-    c.record.p.getExistingList('records').then(l => {
-      // console.log(l.getEntries());
-      if (l.getEntries().length !== 0) throw new Error('Bad list');
-    });
-  }));
-test('deleteListedRecordP', () =>
-  cc.record.deleteListedRecordP('cars', tId).then(ok => {
-    if (!ok) throw new Error('Not ok');
-    cc.record.p.getExistingList('cars').then(l => {
-      // console.log(l.getEntries());
-      if (l.getEntries().length !== 0) throw new Error('Bad list');
-    });
-  }));
+test('p.deleteListedRecord', async t => {
+  const ok = await c.record.p.deleteListedRecord('records', 'record2');
+  t.ok(ok);
+  const l = await c.record.p.getExistingList('records');
+  // console.log(l.getEntries());
+  t.ok(l.getEntries().length === 0);
+});
+test('deleteListedRecordP', async t => {
+  const ok = await cc.record.deleteListedRecordP('cars', tId);
+  t.ok(ok);
+  const l = await cc.record.p.getExistingList('cars');
+  t.ok(l.getEntries().length === 0);
+});
 
-test('p.setData', () =>
-  c.record.p.setData('record1', 'name', 'Test').then(() =>
-    c.record.p.snapshot('record1').then(r => {
-      if (r.name !== 'Test') throw new Error('Bad record');
-    }),
-  ));
+test('p.setData', async t => {
+  await c.record.p.setData('record1', 'name', 'Test');
+  const r = await c.record.p.snapshot('record1');
+  t.ok(r.name === 'Test');
+});
 
-test('p.deleteRecord', () =>
-  c.record.p.deleteRecord('record1').then(() =>
-    c.record.p.has('record1').then(hasR => {
-      if (hasR) throw new Error('Bad record');
-    }),
-  ));
+test('p.deleteRecord', async t => {
+  await c.record.p.deleteRecord('record1');
+  const hasR = await c.record.p.has('record1');
+  t.ok(!hasR);
+});
 
-test('p.deleteList', () =>
-  c.record.p.deleteList('list1').then(() =>
-    c.record.p.has('list1').then(hasR => {
-      if (hasR) throw new Error('Bad record');
-    }),
-  ));
+test('p.deleteList', async t => {
+  await c.record.p.deleteList('list1');
+  const hasR = await c.record.p.has('list1');
+  t.ok(!hasR);
+});
 
 test('shutdown', t => {
   c.close();
